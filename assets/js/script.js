abae1e56d5a241f065cda7b2acb628a3e0ffa3cb
mythosmystery <axiysfire@gmail.com>
2021-06-07 15:01:42 -0500

    added previous version of portfolio code

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    $('.materialboxed').materialbox();    
+  });
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
